c78302c641b52e7a19ce8b12dc51cfc525d0436c
Joachim Perrin <joachimperrin1@gmail.com>
2025-02-23 17:13:41 +0100

    Add comments to ECS files

--- src/ecs/ECS.hpp
+++ src/ecs/ECS.hpp
@@ -1,12 +1,20 @@
+/**
+ * @file ECS.hpp
+ * @author Joachim Perrin (joachimperrin1@gmail.com)
+ * @brief This file is the entry point of the ECS library, it contains all the necessary includes to use the ECS library.
+ * @note This ECS definition is inspired by the one from the youtube channel "Let's Make Games"
+ * @see https://youtube.com/playlist?list=PLhfAbcv9cehhkG7ZQK0nfIGJC_C-wSLrx&si=NtFsQrEc0Quqj-22.
+ * @version 0.1
+ * @date 2025-02-23
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
 #ifndef ECS_HPP
 #define ECS_HPP
 
 #include "Component.hpp"
 #include "Entity.hpp"
 #include "EntitiesManager.hpp"
-
-// #include <iostream>
-// #include <memory>
-// #include <algorithm>
 
 #endif
