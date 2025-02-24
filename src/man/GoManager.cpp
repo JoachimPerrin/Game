ad857d83bbdb5a5bff9a062759043505e46f86e8
Joachim Perrin <joachimperrin1@gmail.com>
2025-02-24 14:58:12 +0100

    résolution du problème?

---
+++ src/man/GoManager.cpp
@@ -0,0 +1,72 @@
+#include "GoManager.hpp"
+#include "Game.hpp"
+#include "Transform.hpp"
+#include "Collider.hpp"
+#include "Sprite.hpp"
+#include "Projectile.hpp"
+#include "Stat.hpp"
+#include "Label.hpp"
+#include "KeyboardController.hpp"
+
+void GoManager::CreatePlayer(ecs::Entity &player)
+{
+    player.AddComponent<ecs::Transform>(Vector2(500.0f, 700.0f), Vector2(0.0f, 0.0f), Vector2(32.0f, 32.0f), Vector2(3.0f, 3.0f));
+    player.AddComponent<ecs::Sprite>("Robot", true);
+    player.AddComponent<ecs::KeyboardController>();
+    player.AddComponent<ecs::Stat>();
+    player.AddComponent<ecs::CircularCollider>("Robot");
+    player.AddGroup(Game::players);
+}
+
+void GoManager::CreateProjectile(Vector2 position, Vector2 velocity, int range, int speed, ecs::ProjectileType type)
+{
+    auto &projectile(manager->AddEntity());
+    projectile.AddComponent<ecs::Transform>(Vector2(position.x, position.y), Vector2(speed, speed), Vector2(32, 32), Vector2(1, 1));
+    projectile.AddComponent<ecs::Sprite>("projectile", true);
+    if (type == ecs::hostileProjectile)
+    {
+        projectile.GetComponent<ecs::Sprite>().Play("HostileBullet");
+    }
+    else if (type == ecs::allyProjectile)
+    {
+        projectile.GetComponent<ecs::Sprite>().Play("AllyBullet");
+    }
+    else if (type == ecs::chargedProjectile)
+    {
+        projectile.GetComponent<ecs::Sprite>().Play("ChargedBullet");
+    }
+    projectile.AddComponent<ecs::CircularCollider>("projectile");
+    projectile.AddComponent<ecs::Projectile>(range, velocity, type);
+    projectile.AddGroup(Game::projectiles);
+}
+
+void GoManager::CreateEnemy(Vector2 position, ecs::EnemyType type)
+{
+    auto &enemy(manager->AddEntity());
+    enemy.AddComponent<ecs::Transform>(Vector2(position.x, position.y), Vector2(0.0f, 0.0f), Vector2(32.0f, 32.0f), Vector2(3.0f, 3.0f));
+    enemy.AddComponent<ecs::Sprite>("enemy", true);
+    enemy.AddComponent<ecs::Stat>();
+    if (type == ecs::bat)
+    {
+        enemy.GetComponent<ecs::Sprite>().Play("Bat");
+        enemy.AddComponent<ecs::CircularCollider>("Bat");
+    }
+    else if (type == ecs::spider)
+    {
+        enemy.GetComponent<ecs::Sprite>().Play("Spider");
+        enemy.AddComponent<ecs::CircularCollider>("Spider");
+    }
+    else if (type == ecs::snake)
+    {
+        enemy.GetComponent<ecs::Sprite>().Play("Snake");
+        enemy.AddComponent<ecs::CircularCollider>("Snake");
+    }
+    enemy.AddGroup(Game::enemies);
+}
+
+void GoManager::CreateLabel(SDL_Rect bbox, std::string text, std::string font)
+{
+    auto &label(manager->AddEntity());
+    label.AddComponent<ecs::Label>(bbox, text, font);
+    label.AddGroup(Game::labels);
+}
---
+++ src/man/GoManager.hpp
@@ -0,0 +1,36 @@
+/**
+ * @file GoManager.hpp
+ * @author Joachim Perrin (joachimperrin1@gmail.com)
+ * @brief This file contains the definition of the GoManager class, which is a class that manages the creation of game objects.
+ * @version 0.1
+ * @date 2025-02-24
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+#ifndef GoMANAGER__HPP
+#define GoMANAGER__HPP
+
+#include "Vector2.hpp"
+#include "Enemy.hpp"
+#include "Projectile.hpp"
+
+/**
+ * @brief The GoManager class is a class that manages the creation of game objects.
+ *
+ */
+class GoManager
+{
+public:
+    GoManager(ecs::EntitiesManager *man) : manager(man) {}
+
+    void CreatePlayer(ecs::Entity &player);
+    void CreateEnemy(Vector2 position, ecs::EnemyType type);
+    void CreateProjectile(Vector2 position, Vector2 velocity, int range, int speed, ecs::ProjectileType type);
+    void CreateLabel(SDL_Rect bbox, std::string text, std::string font);
+
+private:
+    ecs::EntitiesManager *manager;
+};
+
+#endif
